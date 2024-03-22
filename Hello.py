80fb9250c21aa704f59ee3248f15248779db642d
Van5102 <92318063+Van5102@users.noreply.github.com>
2024-03-22 14:01:41 +0000

    update

--- Hello.py
+++ Hello.py
@@ -1,80 +1,51 @@
-# # Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-# #
-# # Licensed under the Apache License, Version 2.0 (the "License");
-# # you may not use this file except in compliance with the License.
-# # You may obtain a copy of the License at
-# #
-# #     http://www.apache.org/licenses/LICENSE-2.0
-# #
-# # Unless required by applicable law or agreed to in writing, software
-# # distributed under the License is distributed on an "AS IS" BASIS,
-# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# # See the License for the specific language governing permissions and
-# # limitations under the License.
-
-# import streamlit as st
-# from streamlit.logger import get_logger
-
-# LOGGER = get_logger(__name__)
-
-
-# def run():
-#     st.set_page_config(
-#         page_title="Hello",
-#         page_icon="👋",
-#     )
-
-#     st.write("# Welcome to Streamlit! 👋")
-
-#     st.sidebar.success("Select a demo above.")
-
-#     st.markdown(
-#         """
-#         Streamlit is an open-source app framework built specifically for
-#         Machine Learning and Data Science projects.
-#         **👈 Select a demo from the sidebar** to see some examples
-#         of what Streamlit can do!
-#         ### Want to learn more?
-#         - Check out [streamlit.io](https://streamlit.io)
-#         - Jump into our [documentation](https://docs.streamlit.io)
-#         - Ask a question in our [community
-#           forums](https://discuss.streamlit.io)
-#         ### See more complex demos
-#         - Use a neural net to [analyze the Udacity Self-driving Car Image
-#           Dataset](https://github.com/streamlit/demo-self-driving)
-#         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-#     """
-#     )
-
-
-# if __name__ == "__main__":
-#     run()
-
+import io
 import streamlit as st
 import pandas as pd
 import numpy as np
+import chardet  
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
-import plotly.graph_objs as go
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 df_cluster = None
 data_file = None
+df = None
+radio_selection = None
 n = 1
 
-def input_file(data_file,n):
-    data_file = st.file_uploader('Thêm dữ liệu',type=['csv'])
+def read_csv_with_file_uploader():
+    # Create a file uploader widget
+    uploaded_file = st.file_uploader("Upload your CSV file", type=['csv'])
+    
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        # To detect file encoding
+        encoding = chardet.detect(bytes_data)['encoding']
+        # To convert to a string based IO:
+        stringio = io.StringIO(bytes_data.decode(encoding))
+        # To read file as string (assuming it's a CSV file):
+        df = pd.read_csv(stringio)
+        return df
+
+def input_file(data_file,n,radio_selection):
+    data_file = read_csv_with_file_uploader()
     if data_file is not None:
-        df = pd.read_csv(data_file)
+        df = data_file
         st.dataframe(df)
         selected_columns = st.multiselect('Lựa chọn dữ liệu phân cụm', df.columns.to_list())
         if selected_columns:
             df_cluster = pd.DataFrame(df[selected_columns])
             st.dataframe(df_cluster)
             Elbow(df_cluster)
-            n = int(st.number_input('Nhập số cụm', min_value=2, key=int))
-            runKmean(df_cluster,n)
+            if radio_selection == 'K-MEANS':
+                n = int(st.number_input('Nhập số cụm', min_value=2, key=int))
+                runKmean(df_cluster,n)            
+            else:
+                runDbScan(df_cluster) 
+                
         else:
             st.write('No columns selected')
 
@@ -93,7 +64,22 @@ def runKmean(df_cluster,n):
         st.pyplot()
 
 def runDbScan(df_cluster):
-    return df_cluster
+    eps = st.slider('Chọn giá trị eps', min_value=0.1, max_value=5.0, value=0.5, step=0.1)
+    min_samples = st.slider('Chọn giá trị min_samples', min_value=1, max_value=50, value=5, step=1)
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    clusters= dbscan.fit_predict(df_cluster)
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df_cluster.iloc[:, 0], df_cluster.iloc[:, 1], c=clusters, cmap='viridis', marker='o')
+    # centers = dbscan.core_sample_indices_
+    # plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75, marker='x')
+    n_clusters_ = len(set(clusters)) - (1 if -1 in clusters else 0)
+    n_noise_ = list(clusters).count(-1)
+    st.write('Số lượng cụm:', n_clusters_)
+    st.write('Số lượng điểm nhiễu:', n_noise_)
+    plt.title('DBSCAN Clustering')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    st.pyplot()
 
 def Elbow(df_cluster):
     wcss = []
@@ -114,18 +100,13 @@ def run():
     st.set_page_config(
         page_title='Demo Sản Phẩm',
         page_icon='💻',
-    )
+        )
 
-    st.title('Nghiên cứu khai phá dữ liệu và ứng dụng trong phân loại kết quả học tập của sinh viên')
-    input_file(data_file,n)
-    
     with st.sidebar:
         st.title('Menu')
         radio_selection = st.radio('Lựa chọn thuật toán', ['K-MEANS','DBSCAN'])
-        if radio_selection == 'K-MEANS':
-            runKmean(df_cluster,n)
-        else:
-            runDbScan(df_cluster) 
+    st.title('Nghiên cứu khai phá dữ liệu và ứng dụng trong phân loại kết quả học tập của sinh viên')
+    input_file(data_file,n,radio_selection)
         
 # running main func
 if __name__ == '__main__':
